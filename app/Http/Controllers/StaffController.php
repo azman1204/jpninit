3978bd47444a92a47442f3e40bf86e8e69e7453c
Jimmy <azman740412@gmail.com>
2018-11-13 15:26:56 +0800

    quill

--- app/Http/Controllers/StaffController.php
+++ app/Http/Controllers/StaffController.php
@@ -1,9 +1,18 @@
 <?php
 namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Models\Pengguna;
 
 class StaffController extends Controller {
-    public function search() {
-        return view('staff.search');
+    public function search(Request $req) {
+        if($req->has('nama')) {
+            // on click btn search
+            $p = Pengguna::all();
+            return view ('staff.search', compact('p'));
+        } else {
+            // user click on menu staff dir
+            return view('staff.search');
+        }
     }
     
     public function view() {
--- resources/views/staff/search.blade.php
+++ resources/views/staff/search.blade.php
@@ -1,14 +1,44 @@
-@extends('app')
+@extends('quill')
 @section('content')
-<div class="card">
-    <div class="card-header">Direktori Staf</div>
-    <div class="card-body">
-        <div class="row">
-            <div class="col-md-12">
-                Nama : <input type="text" class="form-control">
+<div class=" col-md-6">
+    <div class="card">
+        <div class="card-header">Direktori Staf</div>
+        <div class="card-body">
+            <form method="post" action="{{url('staff/search')}}">
+            @csrf
+            <div class="row">
+                <div class="col-md-12">
+                    Nama <input name="nama" type="text" class="form-control">
+                </div>
+                <div class="col-md-12">
+                    Negeri <select name="negeri" class="form-control"></select>
+                </div>
+                <div class="col-md-12">
+                    Cawangan <select name="cawangan" class="form-control"></select>
+                </div>
+                <div class="col-md-12">
+                    <input type="submit" class="btn btn-primary" value="Cari">
+                </div>
             </div>
+            </form>
         </div>
     </div>
 </div>
-@endsection
 
+@if (isset($p))
+<table class="table table-bordered" style="margin-top: 10px;">
+    <tr>
+        <th>#</th>
+        <th>Nama</th>
+        <th>Email</th>
+    </tr>
+    @foreach($p as $staff)
+    <tr>
+        <td></td>
+        <td>{{$staff->nama}}</td>
+        <td>{{$staff->alamat_emel}}</td>
+    </tr>
+    @endforeach
+</table>
+@endif
+@endsection
